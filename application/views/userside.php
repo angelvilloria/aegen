d3cf8aa17a9317f87883b407898867c6e2aa0763
Ronald Fernando <31946697+RonaldFernando@users.noreply.github.com>
2018-10-31 15:56:51 +0800

    Views Update

--- application/views/userside.php
+++ application/views/userside.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <li class="nav-item mx-0 mx-lg-1">
               <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Archive</a>            
 			<li class="nav-item mx-0 mx-lg-1">
-              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="<?php echo base_url(); ?>index.php/AdminController/logout">Log out</a>
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="<?php echo base_url('logout'); ?>">Log out</a>
 			  
             </li>
           </ul>
