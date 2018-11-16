cc9f466171767488a6d14b9637d665d4caf2d620
Angel Villoria <30525269+angelvilloria@users.noreply.github.com>
2018-11-16 11:39:07 +0800

    updated sql with new tables

--- aegen.sql
+++ aegen.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2018 at 02:14 PM
+-- Generation Time: Nov 16, 2018 at 03:34 AM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -19,6 +19,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `aegen`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chapter`
+--
+
+CREATE TABLE `chapter` (
+  `CSID` varchar(10) NOT NULL,
+  `ChapNum` int(2) NOT NULL,
+  `ChapName` varchar(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `chapter`
+--
+
+INSERT INTO `chapter` (`CSID`, `ChapNum`, `ChapName`) VALUES
+('IT102', 1, 'Introduction to PHP');
 
 -- --------------------------------------------------------
 
@@ -40,20 +59,16 @@ CREATE TABLE `choices` (
 
 CREATE TABLE `coursesyllabus` (
   `CSID` varchar(10) NOT NULL,
-  `CSName` varchar(60) NOT NULL,
-  `TopicID` int(5) NOT NULL,
-  `TopicName` varchar(55) NOT NULL,
-  `ChapNum` int(2) NOT NULL,
-  `ChapName` varchar(60) NOT NULL,
-  `Hours` int(2) NOT NULL
+  `CSName` varchar(60) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `coursesyllabus`
 --
 
-INSERT INTO `coursesyllabus` (`CSID`, `CSName`, `TopicID`, `TopicName`, `ChapNum`, `ChapName`, `Hours`) VALUES
-('IT102', '', 1, 'Intro', 1, 'Introduction to PHP', 2);
+INSERT INTO `coursesyllabus` (`CSID`, `CSName`) VALUES
+('IT102', 'OOP Programming'),
+('IT101', 'IT Fundamentals');
 
 -- --------------------------------------------------------
 
@@ -65,6 +80,14 @@ CREATE TABLE `facultycsid` (
   `FacultyID` varchar(15) NOT NULL,
   `CSID` varchar(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `facultycsid`
+--
+
+INSERT INTO `facultycsid` (`FacultyID`, `CSID`) VALUES
+('15-037-017', 'IT102'),
+('15-037-017', 'IT101');
 
 -- --------------------------------------------------------
 
@@ -89,10 +112,19 @@ CREATE TABLE `questions` (
 --
 
 CREATE TABLE `topic` (
-  `TopicID` int(5) NOT NULL,
   `CSID` varchar(10) NOT NULL,
-  `Topic` varchar(60) NOT NULL
+  `ChapNum` int(2) NOT NULL,
+  `TopicNum` int(2) NOT NULL,
+  `TopicName` varchar(55) NOT NULL,
+  `Hours` int(2) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `topic`
+--
+
+INSERT INTO `topic` (`CSID`, `ChapNum`, `TopicNum`, `TopicName`, `Hours`) VALUES
+('IT102', 1, 1, 'Netbeans IDE Introduction', 2);
 
 -- --------------------------------------------------------
 
@@ -102,6 +134,7 @@ CREATE TABLE `topic` (
 
 CREATE TABLE `user` (
   `FacultyID` varchar(15) NOT NULL,
+  `CSID` varchar(10) NOT NULL,
   `LName` char(25) NOT NULL,
   `FName` char(25) NOT NULL,
   `Password` varchar(32) NOT NULL,
@@ -114,11 +147,11 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`FacultyID`, `LName`, `FName`, `Password`, `College`, `Department`, `IsAdmin`) VALUES
-('15-037-018', 'Cueto', 'Mary Tierce', '953f0e5d387909913f49fce1729edf2f', 'COS', 'MATH', 0),
-('15-037-001', 'Marin', 'Elvin', 'e10adc3949ba59abbe56e057f20f88', 'COS', 'MATH', 0),
-('15-037-017', 'Villoria', 'Angel', 'e10adc3949ba59abbe56e057f20f883e', 'COS', 'MATH', 0),
-('15-037-003', 'Mangaco', 'Aeinreb', '827ccb0eea8a706c4c34a16891f84e7b', 'COE', 'MATH', 0);
+INSERT INTO `user` (`FacultyID`, `CSID`, `LName`, `FName`, `Password`, `College`, `Department`, `IsAdmin`) VALUES
+('15-037-018', '', 'Cueto', 'Mary Tierce', '953f0e5d387909913f49fce1729edf2f', 'COS', 'MATH', 0),
+('15-037-001', '', 'Marin', 'Elvin', 'e10adc3949ba59abbe56e057f20f88', 'COS', 'MATH', 0),
+('15-037-017', '', 'Villoria', 'Angel', 'e10adc3949ba59abbe56e057f20f883e', 'COS', 'MATH', 0),
+('15-037-003', '', 'Mangaco', 'Aeinreb', '827ccb0eea8a706c4c34a16891f84e7b', 'COE', 'MATH', 0);
 
 --
 -- Indexes for dumped tables
@@ -141,12 +174,6 @@ ALTER TABLE `coursesyllabus`
 --
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`QuestionID`);
-
---
--- Indexes for table `topic`
---
-ALTER TABLE `topic`
-  ADD PRIMARY KEY (`TopicID`);
 
 --
 -- Indexes for table `user`
