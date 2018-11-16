48380bbf8d112a93ab72e70a4a574603e4dcf3bd
Angel Villoria <30525269+angelvilloria@users.noreply.github.com>
2018-11-16 11:47:55 +0800

    added more functions for user and cs details

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -20,4 +20,35 @@ class UserModel extends CI_Model
 		$query = $this->db->get("coursesyllabus");
 		return $query;
 		}
+		
+		public function userdetails($facultyid)
+		{
+
+			$this->db->select('*');
+			$this->db->from('user');
+			$this->db->where('FacultyID', $facultyid);
+			$query = $this->db->get();
+			
+			return $query->result_array();
+		}
+		
+		public function get_csid($facultyid)
+		{
+			$this->db->select('CSID');
+			$this->db->from('facultycsid');
+			$this->db->where('FacultyID', $facultyid);
+			$query = $this->db->get();
+			
+			return $query->result_array();
+		}
+		
+		public function csid_details($csid)
+		{
+			$this->db->select('*');
+			$this->db->from('coursesyllabus');
+			$this->db->where('CSID', $csid);
+			$query = $this->db->get();
+			
+			return $query->result_array();			
+		}
 }
