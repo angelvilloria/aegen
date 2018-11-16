4021a6815178f38177140b554a7fa12b407b8b96
Angel Villoria <30525269+angelvilloria@users.noreply.github.com>
2018-11-16 11:43:16 +0800

    get userdetails and name of csid's

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -3,20 +3,43 @@
 class UserController extends CI_Controller {
 
 
-	public function __contruct()
+	public function __construct()
 	{
+		parent::__construct();
+		$this->load->model('UserModel','usermodel');
+		
 		if (!isset($_SESSION['user_logged']))
 		{
-			parent::__contruct();
 			$this->session->set_flashdata("error","Please log in first.");
 			redirect("AdminController/login","refresh");
 		}
 	}
 	public function profile()
 	{
+		//variable initialize
+		$csid_details[] = "";
+		$a = 0;
+		
+		//get faculty id from AdminController
+		$faculty_id = $this->session->flashdata('id');
+		
+		//get userdetails 
+		$userdata = $this->usermodel->userdetails($faculty_id);
+		$data['user'] = $userdata;
+		
+		//get csid of the user
+		$csid = $this->usermodel->get_csid($faculty_id);
+		
+		//get the name of the csid's
+		foreach($csid as $csid){
+			$result = $this->usermodel->csid_details($csid['CSID']);
+			$csid_details[$a] = $result[0];
+			$a = $a + 1;
+		}
+		$data['cs'] = $csid_details;
+
 		$this->load->view('includes/header');
-		$this->load->view('userside');
+		$this->load->view('userside', $data);
 	}
-	
 	
 }
