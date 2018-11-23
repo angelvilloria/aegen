12b4d73e18f61cb6b77851f1e6b93b248d8cc5c2
Angel Villoria <30525269+angelvilloria@users.noreply.github.com>
2018-11-23 20:44:58 +0800

    user and admin's footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,6 +1,42 @@
- <div class="copyright py-4 text-center text-white">
+ <!-- Footer -->
+    <footer class="footer text-center">
       <div class="container">
-        <small>Copyright &copy; Cueto~Fernando~Villoria</small>
+        <div class="row">
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Location</h4>
+            <p class="lead mb-0" style="font-family: Segoe UI;">Manila, Philippines</p>
+          </div>
+          <div class="col-md-4 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Around the Web</h4>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fab fa-fw fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fab fa-fw fa-google-plus-g"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
+                  <i class="fab fa-fw fa-twitter"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-4" >
+            <h4 class="text-uppercase mb-4">About AEGEN</h4>
+            <p class="lead mb-0" style="font-family: Segoe UI;">AEGen is an automated exam generator system which you can item analyze your question and generate questions stored in a databank.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="copyright py-4 text-center text-white">
+      <div class="container">
+        <small style="font-family: Segoe UI;">Copyright &copy; Cueto | Fernando | Villoria 2018</small>
       </div>
     </div>
 
