fc8b14ce2d3671e6979e569878970b5d5fd85ae9
Ronald Fernando <31946697+RonaldFernando@users.noreply.github.com>
2018-11-25 15:44:14 +0800

    Removed Around the web ...

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,6 +1,22 @@
- <div class="copyright py-4 text-center text-white">
+ <!-- Footer -->
+    <footer class="footer text-center">
       <div class="container">
-        <small>Copyright &copy; Cueto~Fernando~Villoria</small>
+        <div class="row">
+          <div class="col-md-6 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Location</h4>
+            <p class="lead mb-0" style="font-family: Segoe UI;">Manila, Philippines</p>
+          </div>
+          <div class="col-md-6" >
+            <h4 class="text-uppercase mb-4">About AEGEN</h4>
+            <p class="lead mb-0" style="font-family: Segoe UI;">AEGen is an automated exam generator system which you can item analyze your question and generate questions stored in a databank.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="copyright py-4 text-center text-white">
+      <div class="container">
+        <small style="font-family: Segoe UI;">Copyright &copy; Cueto | Fernando | Villoria 2018</small>
       </div>
     </div>
 
