84e0e85b8fd74781097a54027d45879bd1af3e8a
Angel Villoria <30525269+angelvilloria@users.noreply.github.com>
2018-12-01 01:15:41 +0800

    updated user model

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -51,4 +51,43 @@ class UserModel extends CI_Model
 			
 			return $query->result_array();			
 		}
+		
+		public function csid_chapters($csid)
+		{
+			$this->db->select('*');
+			$this->db->from('chapter');
+			$this->db->where('CSID', $csid);
+			$query = $this->db->get();
+			
+			return $query->result_array();
+		}
+		
+		public function csid_topics($chapter, $csid)
+		{
+			$array = array('CSID' => $csid, 'ChapNum' => $chapter);
+
+			$this->db->select('*');
+			$this->db->from('topic');
+			$this->db->where($array);
+			$query = $this->db->get();
+			
+			return $query->result_array();
+		}
+		
+		public function save_question($CSID, $ChapNum, $TopicNum, $Question, $QuestionType, $TakersNum, $CorrectNum)
+		{
+			$array = array('CSID' => $CSID, 'ChapNum' => $ChapNum, 'TopicNum' => $TopicNum, 'Question' => $Question, 'QuestionType' => $QuestionType, 'TakersNum' => $TakersNum, 'CorrectNum' => $CorrectNum, 'IsApproved' => 0);
+			
+			$this->db->insert('questions', $array);
+			$insert_id = $this->db->insert_id();
+			
+			return $insert_id;
+		}
+		
+		public function save_choice($QuestionID, $choice)
+		{
+			$array = array('QuestionID' => $QuestionID, 'Choice' => $choice);
+			
+			$this->db->insert('choices', $array);
+		}
 }
